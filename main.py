1bc8fa6a8fa9bc6d7e704b05387b8fe342b612aa
ArmandsBiceks <90174764+ArmandsBiceks@users.noreply.github.com>
2021-10-04 07:11:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Armands Bičeks
+'''
+
+def cipari ():
+  print('pirmā_funkcija')
+cipari()
+
+def cipari (burti):
+  print(cipari, burti)
+  cipari(arguments)
+
+def cipari (burti, grāmatas): 
+  print(cipari, burti, grāmatas) 
+  cipari(arguments, otrais)
+  def cipari(burti, grāmatas ):
+    return burti+grāmatas
+    burti=9
+    grāmatas=6
+    print(cipari(burti+grāmatas))
